7be5330d9ab23f5020a1ec8fd752dd3b261d2b85
Eva Reitzel <evareitzel@gmail.com>
2023-09-06 11:45:34 -0400

    commit tree 95ab8ea9696f4b8c66031844543fb09f7af077db

--- lib/first_spicy_batch.rb
+++ lib/first_spicy_batch.rb
@@ -4,5 +4,33 @@
 # def is_spicy_batch(batch):
 
 def first_spicy_batch(n)
-  # your code here
+
+  p n.class
+  p n
+
+  # found_arr = n.select { |el| el.is_spicy_batch }
+
+  # puts found_arr
+  # puts found_arr.first
+
+  # n.select { |el| el.is_spicy_batch }
 end
+
+
+
+
+  # your code here
+  # spicy = []
+
+  #   spicy << n
+
+  #   p spicy
+  #   p spicy.first
+  # puts n.find_index()
+  # n.find_index(x => x === true)
+  # n
+  
+  # p n
+  # found = n.find { |el| el.is_spicy_batch }
+
+  #   p found
